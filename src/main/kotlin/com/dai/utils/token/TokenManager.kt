248e851efdf78b2e5a01ccb0e5887f790b853980
daihaitao <daihaitao@wanwujieshu.net>
2018-01-30 16:09:39 +0800

    Token

--- src/main/kotlin/com/dai/utils/token/TokenManager.kt
+++ src/main/kotlin/com/dai/utils/token/TokenManager.kt
@@ -15,13 +15,14 @@ open class TokenManager {
     //验证是否是同一个用户
     fun verifyAccount(httpServletRequest: HttpServletRequest) {
         val cookies = httpServletRequest.cookies
-        for (cookie in cookies) {
-            println("cookie maxAge= ${cookie.maxAge}")
-            println("cookie comment= ${cookie.comment}")
-            println("cookie name= ${cookie.name}")
-            println("cookie value= ${cookie.value}")
-            println("cookie secure= ${cookie.secure}")
-        }
+        if (cookies != null)
+            for (cookie in cookies) {
+                println("cookie maxAge= ${cookie.maxAge}")
+                println("cookie comment= ${cookie.comment}")
+                println("cookie name= ${cookie.name}")
+                println("cookie value= ${cookie.value}")
+                println("cookie secure= ${cookie.secure}")
+            }
     }
 
 
