f0797796128dfab329155566ace0728005ea0c0f
daihaitao <daihaitao@wanwujieshu.net>
2018-05-09 09:58:46 +0800

    modify file

--- src/main/kotlin/com/dai/controller/LoginController.kt
+++ src/main/kotlin/com/dai/controller/LoginController.kt
@@ -8,9 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
+import java.io.File
+import java.util.ArrayList
 import javax.servlet.http.Cookie
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+import kotlin.collections.HashMap
+import kotlin.collections.set
 
 /**
  * Created by dai on 2018/1/29.
@@ -95,5 +99,41 @@ constructor(private val loginService: LoginService, private val tokenService: To
 
         }
         return strings
+    }
+
+    var files = ArrayList<File>()
+    @ResponseBody
+    @RequestMapping("/getFiles", method = [(RequestMethod.GET)])
+    fun getMusic(): Any {
+        if (files.size > 0) files.clear()
+        getFileList("/var/workfile/music")
+        return files;
+    }
+
+
+    fun getFileList(path: String) {
+        val file = File(path)
+        try {
+            when {
+                file.isDirectory -> {
+                    val fs = file.listFiles()
+                    (0..fs.size - 3)
+                            .map { fs[it].absolutePath }
+                            .forEach {
+                                getFileList(it)
+                            }
+                }
+                file.isFile -> {
+                    val fName = file.absoluteFile;
+                    files.add(fName)
+                    println("fName.name = ${fName.name}")
+                    println("fName.totalSpace = ${fName.totalSpace}")
+                    println("fName.length = ${fName.length()}")
+                }
+                else -> System.out.println("路径不正确!")
+            }
+        } catch (e: Exception) {
+            println("e.message = $e")
+        }
     }
 }
